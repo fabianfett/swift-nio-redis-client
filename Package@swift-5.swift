6ff4ffbadf8cc48324ee4ab65826475969d6e645
Helge Heß <devteam@zeezide.de>
2019-03-29 17:44:04 +0100

    NIO 2 API: ctx => context
    sigh ...

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", 
                  from: "2.0.0"),
         .package(url: "https://github.com/SwiftNIOExtras/swift-nio-redis.git", 
-                 from: "0.10.0")
+                 from: "0.10.2")
     ],
     targets: [
         .target    (name: "Redis",      dependencies: [ "NIORedis" ]),
